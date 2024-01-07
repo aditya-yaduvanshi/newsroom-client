45fce8bc04fc70a9bf00614f43ca4e15c1a8c993
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-07 19:04:34 +0530

    config: tailwind custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: "rgba(39,35,151,1)",
+        "primary-light": "rgba(55,50,179,1)",
+        secondary: "#f0f5ff",
+        "secondary-light": "#bbc6ff",
+        tertiary: "#ffdb44",
+        quarternary: "#092540",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
