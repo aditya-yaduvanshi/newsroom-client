a6de8eb1fb34c303bc5cddb9d67513fb071a3c21
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-07 19:03:37 +0530

    feat: static posts data

---
+++ consts/posts.ts
@@ -0,0 +1,117 @@
+export const TRENDING_POSTS = [
+  {
+    _id: 1,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhefvQ2Mnu4ZvTL_3l1WFPXfHuAJ1nC5zu8eq5E2vq0TPEpfMEPyjHDa8Z3dMt0iuMwIw5p69v8_9qv86n0Oap19zOmKdLo8pTWFoG34fLrVmfcaUJmUXt7jhpNM4F1kxPhQvKdnYUZR723IFlDFhAj01xEe9etO95vqBgXp3jEDq11lSZ8uXtAEocxozs/w72-h72-p-k-no-nu/nudge.jpg",
+    title: "5 Ways to Reduce SaaS Security Risks",
+  },
+  {
+    _id: 2,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiDC7bOP0IOLlWLJoFkvQGLaYku-ENhqa3f1ejlxrHzIvgibT6FkTRCiP7LocFmgdfEZO-XNcwTcAZBhNe_yNMaVG7uEMKW8UrpFia0HvEUP1hHOAnYjmoNq4-3g1nXf5cH7VGBRaS17fc7UtQ09zdzHDkLDTus0VtuRGW9XqPQLEBAb3hlWjwox1SR1L6j/w72-h72-p-k-no-nu/malware.jpg",
+    title: "New Rugmi Malware Loader Surges with Hundreds of Daily Detections",
+  },
+  {
+    _id: 3,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgYlBfH3yE549vJMDJw3rkUvJWWLK36aNCks4Txk5e0VQjjVdaj9EFGRQuUSUWi-YQSXsT3QYBryi2icNpu8n0SMg1TwmNt1hVRMdMWAkauXRw6gcrxcrhXuKFQW_Uorz8t_x0a-2wP8gt0hkpCZ-8BQGZuCzhRT2u0F3_jGv4hDfWVplbpSDBHFr9C_TYX/w72-h72-p-k-no-nu/smtp.jpg",
+    title:
+      "SMTP Smuggling: New Flaw Lets Attackers Bypass Security and Spoof Emails",
+  },
+  {
+    _id: 4,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiBufqwHLGFsJy0ZPA88aCoaIMxXQfNSmkwhz9mg0s_VCJvvTTFt5p5wW7gG4FJKIMcBkur-1pOtZqaB6qJjPS4KJ8LUIpJZ9yeoKVh5CvtM2WayatSXzOoVGelCQh_3yZ2tITXYi5tsQfCK7Lbc6yrtEhwzQRauyUHvelV7vy7s5EJPZgZZQhxPa6NwKUD/w72-h72-p-k-no-nu/windows-security.jpg",
+    title:
+      "New Variant of DLL Search Order Hijacking Bypasses Windows 10 and 11 Protections",
+  },
+  {
+    _id: 5,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhd37epVak86fy6hACcwGkNleEemhqriOcY81gIwT0_lbWBI3kSlL7ap6mgG4OaNCbfEB4dBld0D-8HOIpv0PmKYrRPmXHI7fq4owkwjAN6DfuTUlS9djRlnlaXQtradYxXhrjeTqGEqUuMyD8M4M1iFVhebjjCfS-FWqYgs6A4VuechclNkFsUO-XpzpQY/w72-h72-p-k-no-nu/tech.jpg",
+    title:
+      "Google Settles $5 Billion Privacy Lawsuit Over Tracking Users in 'Incognito Mode'",
+  },
+  {
+    _id: 6,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi6I8fLfY-g3YLU9G6EpHshxGedvA06mfQC3iZwbjrZHSulq3E4XioWUjgEriajsPNdy7Ys2G72VcIqcWNf5aygYROT6Ukcymot0GQCluzkdnr04ZI07ymwWhRvZIrFWbuhBH1yHmTtZVrq-IREDdW8lEb7AVXZb77xAntYc2jra2Q1Kb6dKMHEZqzIEF00/w72-h72-p-k-no-nu/ssh.jpg",
+    title:
+      "New Terrapin Flaw Could Let Attackers Downgrade SSH Protocol Security",
+  },
+  {
+    _id: 7,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj023lOz4mxJLzv6kqk4FRSQtqt4WMnW4K0j4juu6FlyQswCC55M1A1HfedzO_HuVi3yL2UNqsapuudd7Q3RRhsCgKw3qgxM6CMldamZM6F0-Mxx7g3xYR9ADooee3dNJ-WElTE5gfpVZjSDIY1OzisQqoWjiW9XSHmggCwyrXKL_78FCDz71bbs9Ry9X3l/w72-h72-p-k-no-nu/tokenn.jpg",
+    title:
+      "Malware Using Google MultiLogin Exploit to Maintain Access Despite Password Reset",
+  },
+  {
+    _id: 8,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjldEdWkuCd4yyy3PBv0fBF5a0AcGkmf49ssVCzpBH07nlrh7IrvFBwji9ZvULtpIO3DJQ0Vq3n8wIJobGNzymuJP1xT14btRIewk28KssUuoSw3lyyRkkQrp0qRZ4r5e8Y4Wkm62j1DOskyB_t-bVl09SqZWIY-ZOsrsAPCXN0pRJaLN-TtWqol9jB7q0i/w72-h72-p-k-no-nu/windows.jpg",
+    title: "New Bandook RAT Variant Resurfaces, Targeting Windows Machines",
+  },
+];
+export const POPULAR_POSTS = [
+  {
+    _id: 1,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgJpQ1ULysfyET9ZlJDD3vHvNtshu5cabfSi4LB6zTePFO19yOUwiI7_ADuA02_bEGLmwGEWrpQgygCbQSHooFfIUsTW7EafpAQhMTt6tnlVtoBRnl6OslzTt4-9ZiCnCdI7tNl9DRL6DQA6sfaf28C-_pHkwkBnwbniINwwb8vcDo0XnZ7KDXx1ybJL9u0/s72-rw-ft-e30/anyrun.jpg",
+    title: "Effortless Malware Analysis with Interactive ANY.RUN Sandbox",
+  },
+  {
+    _id: 2,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj9YRhmzK60D-jlk0GLVPBDcYueok9JlkaRWZW2PKJWZbA2jAvmBC79_RWArmp_fSoxzsQM3UXWkw8MN0JZ_OtrT8FQ-Cx_zzx0rx4LM_kqbmQnACP8YLgdhnawBXqoJIR0-WPI0wPzIGuOZZ1XTlR35scb-BPorTm11TyxS5E2oem8_nRW_8NeJNAOmS5d/s72-rw-ft-e30/report.jpg",
+    title:
+      "Ultimate Cybersecurity Roadmap - Download Security Navigator 2024 Now",
+  },
+  {
+    _id: 3,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgSIoaxi3weOf9JoGAeLWqLEWKXbNcoOG7W1HNN4Wmg0FentXF3shBciFostp0i6dNWtGF-AkauOmrdRriQwoorE9lYWSl9WiOupM8xhxPXELXXMo2-Qh1Mzt00woRp5Qv7cUsAtPzNCgOLWqCrL6XC4F7xdOsEvsPjEMuDNIGCAGMhyu95nYvzAMCzdaUM/s72-rw-ft-e30/vero.jpg",
+    title:
+      "Get Free Risk Assessment with Actionable Recommendations and Expert Support",
+  },
+  {
+    _id: 4,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhcsoyca1TQuWhHmvMaPXtfimgoAdBGhKDes_ZC4zoYrZo-8-w4VJ_KjjDVzczJojcF1khTWug28rjjtk_bUOTjyTS5JdLbzCbyybaMICSvk782IkfbIVX6rOh8KYklfgtKCTKD3Fo7H13XsROesok0ydvsVN_JV0tRBVxYAeO3NbPC-heerCKcGvram2ee/s72-rw-ft-e30/wiz.jpg",
+    title: "Secure Azure Like a Pro: Download This Cheat Sheet",
+  },
+];
+export const RESOURCES_POSTS = [
+  {
+    _id: 1,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiUgPKLpatbb1e1v2vmvmiQacYazJqaqTRueIP6JdLGa1UottRKq2VAfn5Q_vEBUpqbptODiC2yNjVG8XAc_UVR36SSbMuSdjnrVcZvfG-ys2szgkc0bLcRHp4aJf3gwdv77A4oDTMlMteJofqWPT8y8geSHjOoVzy_X5lY96yY4fBO99x2O2Hxm7kTUnl-/s260-rw-ft-e30/SMB.png",
+    title: "Empower Your Defense with SMB Threat Insights",
+    description:
+      "Get crucial insights into adversary patterns and behaviors tailored to what's targeting SMBs.",
+  },
+  {
+    _id: 2,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEio06UQtf6w9SYo6gcUpURBOpQQmbakTmtN4XaINDaWWbGKIgw1l5LqVfYQ6wbKI_GuXG2pNi9T8i29pVvtVipq0pPNqZECKfLESB-2F0qGAu3rSNKEgAQCbtiaqoDDzHszbjNmQwyHTIkSgHy6COIUjRUA1GftOQ3lBCYHIRc731fSyNYEUCr1oHMmi9Ta/s260-rw-ft-e30/Cynomi.jpg",
+    title: "MSPs & MSSPs: Start Your vCISO Journey Here",
+    description:
+      "Get best practices on defining your goals, pitfalls to avoid, and a 100 day action plan.",
+  },
+  {
+    _id: 3,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhGlgqD2Bt3H19aX_bB872mYEV-_wrmNTVNxOiwjYcLhaUxLWHg8HCpJNnE8BlpNLs1IgAWUNo0ccdRDMxLjOqXPwMlz6VyTMbCl55QP23fL2kHCzWbVPvxgW8hZoWRTJ99AQAKKWT_pii5dU6Pfth4rGuZCGDUZDhI_v-NJiTOMXFA0RKHYue1IUj-K5kf/s260-rw-ft-e30/threatLocker.jpg",
+    title: "ThreatLocker® Redefines Incident Response with Zero Trust",
+    description:
+      "Learn how ThreatLocker® can help respond to active cyberattacks and stop malware from spreading",
+  },
+  {
+    _id: 4,
+    thumbnail:
+      "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEir4JPuHbPXO3LxZYWSU-xwpZg4_zEL8WEmn_cMABqSXnsyHcl7Pa7EvfG1Kla3qXvnUSZ9qKO72oMbQZKyg0L3POclOmCsiNUyseL-0hNS-1J4oAIZBb4Q8-qYvmDCRj0ONPwjCZTsd_KQAfhECg-PxAS7hu7qpCyzGYCYIMkNiY3jYafhkUXtwSn8QpT-/s260-rw-ft-e30/yy.jpg",
+    title: "Earn a Master's in Cybersecurity Risk Management",
+    description:
+      "Lead the future of cybersecurity risk management with an online Master's from Georgetown.",
+  },
+];
