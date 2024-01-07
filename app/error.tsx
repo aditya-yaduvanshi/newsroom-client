3b30492d1916dff6bf164aebc9c78560507b4aee
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-07 19:07:34 +0530

    feat: init dummy loading and error fallback

---
+++ app/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const ErrorFallback = () => {
+  return <>Error Fallback</>;
+};
+
+export default React.memo(ErrorFallback);
---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const Loading = () => {
+  return <>Loading</>;
+};
+
+export default React.memo(Loading);
