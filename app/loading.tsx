44337f5edd8bbaebc15439b4cd2c1506356f101c
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-08 12:07:06 +0530

    feat: loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,18 @@
 "use client";
 
 import React from "react";
+import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 const Loading = () => {
-  return <>Loading</>;
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center gap-5 p-5">
+        <Icon icon={faSpinner} className="w-48 h-48 animate-spin-slow" />
+        <p className="text-3xl">Loading...</p>
+      </div>
+    </>
+  );
 };
 
 export default React.memo(Loading);
